f9f60229c5cbb443b8055ef764849cdf3758fc99
Aysun Panahli <aysunpanahli58@gmail.com>
2026-08-27 13:24:36 +0400

    Integrated frontend with backend

---
+++ MCV-Mini Project/Controllers/CoursesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MCV_Mini_Project.Controllers
+{
+    public class CoursesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- MCV-Mini Project/obj/Debug/net8.0/MCV-Mini Project.AssemblyInfo.cs
+++ MCV-Mini Project/obj/Debug/net8.0/MCV-Mini Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MCV-Mini Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e412c42a677f170e8918e5872ece54e0e7311fa4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+722f658502fbff81406ec7bca32fba3a30cca2a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("MCV-Mini Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MCV-Mini Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
